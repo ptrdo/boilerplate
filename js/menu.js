9b11943d646c4aeb0752fc936b4c066c729cc503
Peter Sylwester <ptrdo@users.noreply.github.com>
2017-10-20 17:16:29 -0700

    Initial commit

---
+++ js/menu.js
@@ -0,0 +1,123 @@
+﻿(function (exports) {
+  /**
+   * MENU is a class for instantiating a construct of classic <menu><ul><li> hierarchy.
+   * Based on HTML5 standards.
+   *
+   * Note: No dependencies here (e/g jQuery) since menuing may be required in catastrophic circumstances.
+   *
+   * USAGE: new Menu(targetNode, dataCollection);
+   *
+   * @param {HTMLElement} targetNode // reference to an element configured as a template
+   * @param {Array} dataCollection // conventional JSON collection of objects @see app/navigation.js
+   * @returns {this}
+   *
+   */
+  var draw = function(targetNode, dataCollection) {
+
+    var info = dataCollection;
+    var element = targetNode;
+
+    var factory = function (json) {
+
+      // var menu = document.createElement("li"); // TODO: parentNode option (a wrapping element)
+      var drop = document.createElement("menu");
+      var list = document.createElement("ul");
+
+      json.childNodes.forEach(function (child) {
+
+        var text, cell = null, item = document.createElement("li");
+
+        if ("element" in child) {
+          cell = document.createElement(child.element);
+          text = document.createTextNode(child.content);
+          cell.appendChild(text);
+          item.appendChild(cell);
+          if (/\ba\b/i.test(child.element)) {
+            cell.setAttribute("tabindex", 0);
+          }
+        }
+
+        if ("attributes" in child) {
+          for (var prop in child.attributes) {
+            if (child.attributes.hasOwnProperty(prop)) {
+              if (!!cell) {
+                cell.setAttribute(prop, child.attributes[prop]);
+              } else {
+                item.setAttribute(prop, child.attributes[prop]);
+              }
+            }
+          }
+        }
+
+        if ("childNodes" in child) {
+
+          cell = document.createElement("a");
+          text = document.createTextNode("\uFF0B");
+          cell.setAttribute("href", "#void");
+          cell.appendChild(text);
+          cell.classList.add("expander");
+          item.appendChild(cell);
+
+          item.classList.add("more");
+          item.appendChild(factory({"childNodes": child.childNodes}));
+        }
+
+        list.appendChild(item);
+
+      });
+
+      drop.appendChild(list);
+      // menu.appendChild(drop);
+
+      drop.classList.add("dropdown");
+
+      return drop;
+    };
+  }
+
+  var init = function () {
+
+    // TODO: provide option for append/prepend
+    // element = targetNode.appendChild(factory(info));
+
+    element = targetNode.insertBefore(factory(info), targetNode.firstChild);
+    var mask = document.createElement("div");
+    mask.classList.add("menuMask");
+    targetNode.insertBefore(mask, targetNode.firstChild);
+  };
+
+  var remove = function () {
+    var parent = element.parentNode;
+    parent.removeChild(parent.firstChild);  // Remove mask
+    parent.removeChild(parent.firstChild);  // Remove element
+    element = undefined;
+  };
+
+
+  exports.Menu = function (options) {
+    "use strict";
+
+    // private instance properties
+
+    var config = options || {info: 0};
+    var property = config.info; // example
+    var element, source;
+
+    // public instance properties
+
+    var api = {};
+
+    api.render = function (ele, data) {
+
+      element = ele;
+      source = data;
+
+      if (typeof data === "object") {
+        draw(ele, data);
+      }
+    };
+
+    return api;
+
+  };
+}(typeof exports === "object" && exports || this));
---
+++ js/navigation.js
@@ -0,0 +1,205 @@
+﻿"use strict";
+
+/** NAVIGATION
+ *  Data for populating primary menu navigation.
+ *  Consumed by "/app/components/menu/menu"
+ *  Executed by "/app/shell/js/index"
+ *
+ *  NOTE: Modules are variably navigatable --
+ *  sometimes in several ways and sometimes not at all.
+ *  Dynamically prescribing a navigation menu is challenging,
+ *  at least until user needs are better understood.
+ */
+var navigation = {
+  "parentNode": null,
+  "element": "menu",
+  "attributes": {
+    "class": "dropdown"
+  },
+  "childNodes": [{
+    "element": "a",
+    "content": "Home",
+    "attributes": {
+      "href": "#home/lessons/"
+    }
+  }, {
+    "element": "a",
+    "content": "Go to my to-do list",
+    "attributes": {
+      "href": "#home/lessons/todo"
+    }
+  }, {
+    "attributes": {
+      "class": "spacer"
+    }
+  }, {
+    "element": "a",
+    "content": "Getting started",
+    "attributes": {
+      "href": "#",
+      "onclick": "return false;"
+    },
+    "childNodes": [
+    {
+        "element": "a",
+        "content": "Introduction to this app",
+        "attributes": {
+          "href": "#intro",
+          "title": "Introduction to the app functionality"
+        }
+      },
+      {
+        "element": "a",
+        "content": "JSON and programming",
+        "attributes": {
+          "href": "#programming"
+        }
+      }
+    ]
+  }, {
+    "element": "a",
+    "content": "Introduction to modeling",
+    "attributes": {
+      "href": "#",
+      "onclick": "return false;"
+    },
+    "childNodes": [
+      {
+        "element": "a",
+        "content": "SIR models",
+        "attributes": {
+          "href": "#SIR-basics",
+          "title": "the basics of SIR modeling"
+        }
+      }, {
+        "element": "a",
+        "content": "EMOD as an SIR model",
+        "attributes": {
+          "href": "#DTK-as-SIR",
+          "title": "configure EMOD to parallel SIR dynamics"
+        }
+      }
+    ]
+  },{
+    "element": "a",
+    "content": "Population dynamics",
+    "attributes": {
+      "href": "#",
+      "onclick": "return false;"
+    },
+    "childNodes": [
+      {
+        "element": "a",
+        "content": "Population initialization",
+        "attributes": {
+          "href": "#PopulationInitialization",
+          "title": "configuring an initial population"
+        }
+      }, {
+        "element": "a",
+        "content": "Vital dynamics",
+        "attributes": {
+          "href": "#VitalDynamics",
+          "title": "configuring vital dynamics features"
+        }
+      }
+    ]
+  },{
+    "element": "a",
+    "content": "Transmission",
+    "attributes": {
+      "href": "#",
+      "onclick": "return false;"
+    },
+    "childNodes": [
+      {
+        "element": "a",
+        "content": "Intrahost infection",
+        "attributes": {
+          "href": "#intrahost",
+          "title": "dynamics of infection and reinfection within a single host"
+        }
+      },
+      {
+        "element": "span",
+        "content": "Outbreak",
+        "attributes": {
+          "title": "in development"
+        }
+      },
+      {
+        "element": "span",
+        "content": "Population-level transmission",
+        "attributes": {
+          "title": "in development"
+        }
+      }
+    ]
+  },
+  {
+    "element": "a",
+    "content": "Campaigns",
+    "attributes": {
+      "href": "#",
+      "onclick": "return false;"
+    },
+    "childNodes": [
+      {
+        "element": "span",
+        "content": "Campaign events",
+        "attributes": {
+          "title": "in development"
+        }
+      },
+      {
+        "element": "span",
+        "content": "Interventions",
+        "attributes": {
+          "title": "in development"
+        }
+      }
+    ]
+  },{
+    "element": "a",
+    "content": "Model complexity",
+    "attributes": {
+      "href": "#",
+      "onclick": "return false;"
+    },
+    "childNodes": [
+      {
+        "element": "span",
+        "content": "Individual and node properties",
+        "attributes": {
+          "title": "in development"
+        }
+      },
+      {
+        "element": "span",
+        "content": "Migration",
+        "attributes": {
+          "title": "in development"
+        }
+      },
+      {
+        "element": "span",
+        "content": "Dtk-Tools",
+        "attributes": {
+          "title": "in development"
+        }
+      }
+    ]
+  },{
+    "attributes": {
+      "class": "spacer"
+    }
+  }, {
+    "element": "a",
+    "content": "Sign Out",
+    "attributes": {
+      "href": "#signout"
+    }
+  }]
+};
+
+export default navigation;
